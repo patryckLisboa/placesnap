d14580ce0c39d24b6eff025a1b5372c9afaa730e
patryckLisboa <patryck-lisboa@hotmail.com>
2024-02-24 22:16:21 -0300

    teste de endereço

--- src/app/components/apresentacao/bruno/bruno.component.ts
+++ src/app/components/apresentacao/bruno/bruno.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-bruno',
   templateUrl: './bruno.component.html',
-  styleUrls: ['./bruno.component.scss']
+  styleUrls: ['./bruno.component.scss'],
 })
 export class BrunoComponent {
-
+  constructor(private route: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params) => {
+      if (params['id']) {
+        this.router.navigate([`/${params['id']}`]);
+      }
+    });
+  }
 }

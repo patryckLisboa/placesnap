227d04f7a82f623160b66705a4b66bef2ec56591
patryckLisboa <patryck-lisboa@hotmail.com>
2024-02-28 23:14:29 -0300

    ajustes vizuais

--- src/app/components/apresentacao/andressa/andressa.component.ts
+++ src/app/components/apresentacao/andressa/andressa.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-andressa',
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./andressa.component.scss'],
 })
 export class AndressaComponent {
+  larguraTela = window.innerWidth;
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.larguraTela = window.innerWidth;
+  }
+
+  getZoom() {
+    if(this.larguraTela < 350){
+      return this.larguraTela / 350;
+    }
+    return 1
+  }
 }
--- src/app/components/apresentacao/andressa/fourth/fourth.component.ts
+++ src/app/components/apresentacao/andressa/fourth/fourth.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class FourthComponent {
 
+  openWhatsApp(element: HTMLElement) {
+    this.elasticEffect(element);
+    const whatsAppUrl = `https://chat.whatsapp.com/KpJLi94io9LCuyXTZ7cwi9`;
+  
+    setTimeout(() => {
+      window.open(whatsAppUrl, '_blank');
+    }, 500);
+  }
+
+  elasticEffect(element: HTMLElement) {
+    element.classList.add('clicked-elastic');
+    setTimeout(() => {
+      element.classList.remove('clicked-elastic');
+    }, 1000);
+  }
 }

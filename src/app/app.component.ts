22ee0dedafc9616ab53897aef8f96876c7e80f5d
patryckLisboa <patryck-lisboa@hotmail.com>
2024-03-07 20:44:48 -0300

    ajustes nos direcionamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { RedirectService } from './services/redirect/redirect.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'placesnap';
+  constructor(private redirectService: RedirectService) {}
+
+  ngOnInit(): void {
+    this.redirectService.verificarDominio();
+  }
 }
---
+++ src/app/services/redirect/redirect.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RedirectService {
+  constructor(private location: Location, private router: Router) {}
+
+  public verificarDominio(): void {
+    const dominioAtual = this.location.hostname;
+    switch (dominioAtual) {
+      case 'mentoriadressapedrozza.com.br':
+        this.router.navigate(['/andressa']);
+        break;
+      case 'ckya.com.br':
+        this.router.navigate(['/bruno']);
+        break;
+      default:
+        // Nenhum redirecionamento
+    }
+  }
+}

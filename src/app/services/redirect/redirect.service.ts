7f9e8b5300a68f2410e3437c48262a3be19c2af1
patryckLisboa <patryck-lisboa@hotmail.com>
2024-03-19 18:11:52 -0300

    ajuste nas regras de redirecionamento

--- src/app/services/redirect/redirect.service.ts
+++ src/app/services/redirect/redirect.service.ts
@@ -3,10 +3,13 @@ import { Router } from '@angular/router';
 import { MetaInfoService } from '../metaInfo/meta-info.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RedirectService {
-  constructor(private router: Router, private metaInfoService: MetaInfoService) {}
+  constructor(
+    private router: Router,
+    private metaInfoService: MetaInfoService
+  ) {}
 
   public verificarDominio(): void {
     const dominioAtual = window.location.hostname;
@@ -19,15 +22,22 @@ export class RedirectService {
       case 'mentoriadressapedrozza.com.br':
         this.router.navigate(['/andressa']);
         this.metaInfoService.setTitle('Andressa Pedrozza Mentoria');
-        this.metaInfoService.setIcon('./assets/img/andressa/11zon_cropped (1).png');
+        this.metaInfoService.setIcon(
+          './assets/img/andressa/11zon_cropped (1).png'
+        );
         break;
       case 'ckya.com.br':
-        this.router.navigate(['/placesnap']);
+        this.router.navigate(['/bruno']);
+        this.metaInfoService.setTitle('PlaceSnap');
+        this.metaInfoService.setIcon('./assets/img/placesnap.jpg');
+        break;
+      case 'placesnap.netlify.app':
+        this.router.navigate(['/bruno']);
         this.metaInfoService.setTitle('PlaceSnap');
         this.metaInfoService.setIcon('./assets/img/placesnap.jpg');
         break;
       default:
-        // Nenhum redirecionamento
+      // Nenhum redirecionamento
     }
   }
 
